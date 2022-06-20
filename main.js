6f4fef6ab245c9cfae44c35df2e7dbfa79d1711c
morganbutton <morganbuttongithub@gmail.com>
2022-06-19 20:52:10 -0400

    styling

---
+++ main.js
@@ -0,0 +1,18 @@
+let panels = document.querySelectorAll('.panel');
+console.log(panels);
+
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () =>{
+        removeActiveClasses();
+        panel.classList.add("active");  
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
+
+
